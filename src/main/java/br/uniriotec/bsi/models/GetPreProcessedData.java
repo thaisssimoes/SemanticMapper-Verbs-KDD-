67f09c7e20fb930240f253716f6235e19cb5b137
Thaisssimoes <thais.simoes@uniriotec.br>
2017-11-19 13:10:10 -0200

    Method refactoring

--- src/main/java/br/uniriotec/bsi/models/GetPreProcessedData.java
+++ src/main/java/br/uniriotec/bsi/models/GetPreProcessedData.java
@@ -3,49 +3,32 @@ package br.uniriotec.bsi.models;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 import org.apache.commons.io.IOUtils;
 
-import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
-import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
-import edu.stanford.nlp.ling.CoreLabel;
-import edu.stanford.nlp.pipeline.Annotation;
-import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import edu.stanford.nlp.process.CoreLabelTokenFactory;
-import edu.stanford.nlp.process.PTBTokenizer;
-import edu.stanford.nlp.process.WordToSentenceProcessor;
-import edu.stanford.nlp.util.CoreMap;
 
 public class GetPreProcessedData {
 	
-	protected StanfordCoreNLP pipeline;
-
-
 	public String readingPreProcessedText(String filePath) throws FileNotFoundException, IOException {
 		
 		try(FileInputStream preProcessedText = new FileInputStream(filePath)) {     
 		    String preProcessedTextRead = IOUtils.toString(preProcessedText, StandardCharsets.UTF_8);
+		    preProcessedTextRead = preProcessedTextRead.replaceAll("\t", " ");
 		    return preProcessedTextRead;
 		}		
 		
 	}
 
-	
-
-	
-	
-	
-	public void metodoTeste(String textoTeste) throws NoSuchElementException {
-		textoTeste = textoTeste.replaceAll("\t", " ");
-		Scanner scannerTeste1 = new Scanner(textoTeste);
-		Set<String> listaTeste = new HashSet<String>();
 		
-		while(scannerTeste1.hasNextLine()) {
-			String currentWord = scannerTeste1.next();
+	public Set<String> getLabeledVerbs(String preProcessedText) throws NoSuchElementException {
+		
+		Scanner preProcessedTextRead = new Scanner(preProcessedText);
+		Set<String> setOfVerbs = new HashSet<String>();
+		
+		while(preProcessedTextRead.hasNext()) {
+			String currentWord = preProcessedTextRead.next();
 			
 			if ((currentWord.equals("VB"))
 				||(currentWord.equals("VBZ"))
@@ -53,52 +36,25 @@ public class GetPreProcessedData {
 				||(currentWord.equals("VBG"))
 				||(currentWord.equals("VBN"))
 				||(currentWord.equals("VBP"))) {
-				listaTeste.addAll(Arrays.asList(scannerTeste1.nextLine().split(" ")));
-				System.out.println(currentWord + "\nOLAAAAAAAAAAAAAAAAAAAAAAA\n" + listaTeste  + "\nOLAAAAAAAAAAAAAAAAAAAAAAA\n");
-			}
-			else
-				System.out.println(currentWord);
-
-			
-			
-			
+				setOfVerbs.addAll(Arrays.asList(preProcessedTextRead.nextLine().split(" ")));
+			}		
 			
 		}
+		return setOfVerbs;
 		
 	}
 	
-//	public ArrayList<String> searchingForVerbs(String preProcessedText){
-//		ArrayList<String> listOfSentences = new ArrayList<String>();
-//		preProcessedText = preProcessedText.replaceAll("\t", " ");
-//		
-//		Scanner preProcessedTextRead = new Scanner(preProcessedText);
-//		
-//		
-//		String currentLine = preProcessedTextRead.nextLine();
-//		
-//	
-//		
-//		
-//		
-//		while(preProcessedTextRead.hasNextLine()) {
-//
-//			String[] arrayOfWordsInASentence = preProcessedTextRead.nextLine().split(" ");
-//			listOfSentences.addAll(Arrays.asList(arrayOfWordsInASentence));
-//			preProcessedTextRead.nextLine();
-//			System.out.println(listOfSentences);
-//		
-//		}
-//				
-//		return listOfSentences;
-//	}
-//	
-//	
-//	public String getNextWord(String text) {
-//		Scanner currentWord = new Scanner(text);
-//		
-//
-//		return currentWord.next();
-//		
-//	}
 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
